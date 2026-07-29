67aa18b9f77c8b035422bec6afa28a767090f38a
Vansh Rawat <vanshrawat090@gmail.com>
2026-07-29 23:32:36 +0530

    Database inspection modification.

--- Scripts/inspect_database.py
+++ Scripts/inspect_database.py
@@ -1,16 +1,32 @@
 import sqlite3
+
 from config import DATABASE_PATH
 
 conn = sqlite3.connect(DATABASE_PATH)
+
 cursor = conn.cursor()
 
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-tables = [row[0] for row in cursor.fetchall()]
+cursor.execute(
+    "SELECT name FROM sqlite_master WHERE type='table';"
+)
+
+tables = cursor.fetchall()
 
 for table in tables:
-    print(f"\n===== {table} =====")
-    cursor.execute(f"PRAGMA table_info('{table}')")
-    for column in cursor.fetchall():
+
+    table_name = table[0]
+
+    print("\n" + "=" * 50)
+    print(table_name)
+    print("=" * 50)
+
+    cursor.execute(
+        f"PRAGMA table_info('{table_name}')"
+    )
+
+    columns = cursor.fetchall()
+
+    for column in columns:
         print(column)
 
 conn.close()
