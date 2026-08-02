bed0766625c8a2b4acdd1e4203baaafc9b17a701
Vansh Rawat <vanshrawat090@gmail.com>
2026-08-02 21:43:33 +0530

    modification

--- Scripts/inspect_database.py
+++ Scripts/inspect_database.py
@@ -2,31 +2,31 @@ import sqlite3
 
 from config import DATABASE_PATH
 
-conn = sqlite3.connect(DATABASE_PATH)
 
-cursor = conn.cursor()
+def main():
+    with sqlite3.connect(DATABASE_PATH) as conn:
+        cursor = conn.cursor()
 
-cursor.execute(
-    "SELECT name FROM sqlite_master WHERE type='table';"
-)
+        cursor.execute(
+            "SELECT name FROM sqlite_master WHERE type='table';"
+        )
+        tables = cursor.fetchall()
 
-tables = cursor.fetchall()
+        for table in tables:
+            table_name = table[0]
 
-for table in tables:
+            print("\n" + "=" * 50)
+            print(table_name)
+            print("=" * 50)
 
-    table_name = table[0]
+            cursor.execute(
+                f"PRAGMA table_info('{table_name}')"
+            )
+            columns = cursor.fetchall()
 
-    print("\n" + "=" * 50)
-    print(table_name)
-    print("=" * 50)
+            for column in columns:
+                print(column)
 
-    cursor.execute(
-        f"PRAGMA table_info('{table_name}')"
-    )
 
-    columns = cursor.fetchall()
-
-    for column in columns:
-        print(column)
-
-conn.close()
+if __name__ == "__main__":
+    main()
